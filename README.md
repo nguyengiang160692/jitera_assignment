431b8d6efa588d2d1d4ca686bf2dbefeb9b20b3f
Giang Nguyen <nguyengiang160692@gmail.com>
2023-05-16 16:51:28 +0700

    [Database] Update using salt to hash

--- README.md
+++ README.md
@@ -17,5 +17,25 @@ cp .env.example .env
 npm run dev
 ```
 
+Create new database
+```bash
+mongosh
+
+use admin
+# Login with your credentials
+db.auth('<username>', '<password>')
+
+use jitera_db
+
+db.createUser({
+  user: 'jitera_user',
+  pwd: 'jitera_password',
+  roles: [
+    { role: 'readWrite', db: 'jitera_db' },
+  ],
+})
+
+```
+
 
 ### Begin
--- backend/src/model/model.ts
+++ backend/src/model/model.ts
@@ -1,1 +1,41 @@
-import mongoose from 'mongoose';
+import mongoose, { CallbackError } from 'mongoose';
+import bcrypt from 'bcrypt'
+
+export interface IUser extends mongoose.Document {
+    username: string;
+    password: string;
+    salt?: string;
+}
+
+export const UserSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, require: true },
+    salt: { type: String, require: true }
+});
+
+//pre save User to database make sure password is hashed and salted
+const saltRounds = 10;
+
+UserSchema.pre<IUser>('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+
+    try {
+        const salt = bcrypt.genSaltSync(saltRounds);
+
+        return bcrypt.hash(this.password, salt, (err: CallbackError, hash: string) => {
+            if (err) {
+                return next(err);
+            }
+
+            this.password = hash;
+            this.salt = salt;
+
+            return next();
+
+        });
+    } catch (err: any) {
+        return next(err);
+    }
+})
